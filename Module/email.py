751d045a85364bfa7134436006e6b6836ca48a99
Dextron03 <brailyrs03@gmail.com>
2024-11-24 01:35:56 -1200

    Agrege la funcion send_templates_html para plantillas html por correo. Ej: codgios de verificacion, enlaces de activacion, etc. Tambien la libreria/motor de plantillas Jinja2.

--- Module/email.py
+++ Module/email.py
@@ -1,6 +1,10 @@
+import os
 import smtplib
+import mimetypes
 from email.message import EmailMessage
 from email.mime.text import MIMEText
+from jinja2 import Environment, FileSystemLoader
+
 
 class Email:
     def __init__(self, sender:str, receptor:str):
@@ -26,6 +30,7 @@ class Email:
         except smtplib.SMTPException as e:
             print(f"Error en el envío de correo: {e}")
             raise e
+        print("Correo enviado con éxito.")
             
     def send_simple_message(self,subject:str, message:str):
         simple_msg = self.msg
@@ -35,4 +40,24 @@ class Email:
         simple_msg['Subject'] = subject
         
         self.connect_to_server(simple_msg)
+    
+    def send_templates_html(self, subject:str, message:str, context:dict, path_template_dir:str, path:str):
+        msg_wfile = self.msg
+        msg_wfile.set_content(MIMEText(message))
+        msg_wfile['From'] = self.sender
+        msg_wfile['To'] = self.receptor
+        msg_wfile['Subject'] = subject
         
+        # Cargar y renderizar la plantilla HTML usando Jinja2
+        template_dir = os.path.abspath(path_template_dir)  # Carpeta donde tienes la plantilla
+        env = Environment(loader=FileSystemLoader(template_dir))
+        template = env.get_template(path)  # Nombre de tu archivo de plantilla
+        
+        # Renderizar la plantilla con el contexto
+        html_content = template.render(context)
+        
+        # Establecer el contenido del correo como HTML
+        msg_wfile.add_alternative(html_content, subtype='html')
+        
+        # Enviar el correo usando SMTP
+        self.connect_to_server(msg_wfile)
